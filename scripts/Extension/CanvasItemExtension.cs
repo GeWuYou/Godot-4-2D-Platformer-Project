54469ece0d18d6b544eed2b1655877ac644935e8
gewuyou <1063891901@qq.com>
2025-04-09 23:59:43 +0800

    feat(core): add CanvasItem extension function and remove NodeExtension

---
+++ scripts/Extension/CanvasItemExtension.cs
@@ -0,0 +1,35 @@
+﻿
+using Godot;
+
+namespace Godot42DPlatformerProject.scripts.Extension;
+
+/// <summary>
+/// CanvasItemExtension
+/// </summary>
+public static class CanvasItemExtension
+{
+    /// <summary>
+    /// Set the active state of the CanvasItem
+    /// </summary>
+    /// <param name="item"> The CanvasItem to set </param>
+    /// <param name="active"> The active state to set </param>
+    public static void SetActive(this CanvasItem item, bool active)
+    {
+        // 设置不可见
+        item.Visible = active;
+        // 不处理输入
+        item.SetProcess(active);
+        // 不处理物理
+        item.SetPhysicsProcess(active);
+    }
+
+    /// <summary>
+    /// Get the active state of the CanvasItem
+    /// </summary>
+    /// <param name="item"> The CanvasItem to check </param>
+    /// <returns></returns>
+    public static bool IsActive(this CanvasItem item)
+    {
+        return item.Visible&&item.IsPhysicsProcessing()&&item.IsProcessing();
+    }
+}
